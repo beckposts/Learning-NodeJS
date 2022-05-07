94e3ae224202ce19551f931683fd567d8ef79e9f
doordion <doordion@gmail.com>
2022-05-07 10:03:42 +0530

    Module

---
+++ module.js
@@ -0,0 +1,11 @@
+// NPM Modules
+
+const color = require("cli-color");
+
+console.log(color.red("Hello, I'm Beck"));
+
+// Local Module
+const auth = require("./auth");
+
+auth.register("Tiranga Sheetal Beck");
+auth.login("Tirnga", "You You");
